aced19c11d7cc25a86a8f13808276449df9ab35f
kastriotsalihu1 <kastriotsalihu@student.uni-pr.edu>
2020-11-28 20:24:57 +0100

    addedSomeOnHoverStyleChanges

--- root/scripts/main.js
+++ root/scripts/main.js
@@ -2,3 +2,11 @@ function msg(){
     alert("Welcome to the main page");  
    }  
    
+
+
+$(function() {
+    $( 'nav .navigation-list li' ).on( 'click', function() {
+          $( this ).parent().find( 'a.active' ).removeClass( 'active' );
+          $( this ).children().addClass( 'active' );
+    });
+});
