e74cbb8c01af9c66aa331a5e0be0573652a49f75
kastriotsalihu1 <kastriotsalihu@student.uni-pr.edu>
2020-12-16 23:50:56 +0100

    ChangePagesWithinApplication

--- root/scripts/main.js
+++ root/scripts/main.js
@@ -1,13 +1,41 @@
-function msg() {
-    alert("Welcome to the main page");
+const pageNames = {
+    home: "home-content",
+    cursor: "cursor-content",
+    heart: "heart-content",
+    university: "university-content",
+    envelope: "envelope-content",
+    folder: "folder-content",
+    laptop: "laptop-content",
+    map: "map-content",
+    chart: "chart-content",
+    hourglass: "hourglass-content",
+    id: "id-content",
+    newspaper: "newspaper-content",
 }
-
-
+var toggleDuration = 800;
+var activePageName;
+const pages = document.getElementsByClassName("side-button");
 
 $(function() {
     $('.side-button').on('click', function() {
-        $(this).parent().find('.active').removeClass('active');
-        $(this).addClass('active');
-        $('.card-1').css('background-color', 'rgb(' + (Math.random() * 256) + ',' + (Math.random() * 256) + ',' + (Math.random() * 256) + ')');
+        let previousButton = $(this).parent().find('.active');
+        let previousPage;
+
+        if (previousButton.attr("data-value") != $(this).attr("data-value")) {
+
+            previousButton.removeClass('active');
+            $(this).addClass('active');
+
+            previousPage = previousButton.attr("data-value");
+            activePageName = $(this).attr("data-value");
+
+            $(`.${pageNames[previousPage]}`).toggle(toggleDuration);
+            $(`.${pageNames[activePageName]}`).toggle(toggleDuration);
+
+            $(`.${pageNames[activePageName]} .card-1`).css('background-color', `rgb(${Math.random() * 256},${Math.random() * 256},${Math.random() * 256})`);
+            console.log("Active page name:", pageNames[activePageName]);
+        } else {
+            console.log("already there");
+        }
     });
 });
