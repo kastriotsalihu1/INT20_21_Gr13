7b560bbdba1d2bf3130447c57b07d8c8f1c47af1
kastriotsalihu1 <kastriotsalihu@student.uni-pr.edu>
2020-12-16 02:24:46 +0100

    addedSideIconsAndFunctionality

--- root/scripts/main.js
+++ root/scripts/main.js
@@ -1,12 +1,13 @@
-function msg(){  
-    alert("Welcome to the main page");  
-   }  
-   
+function msg() {
+    alert("Welcome to the main page");
+}
 
 
-$(function() {0
-    $( '.sidebar' ).on( 'click', function() {
-          $( this ).parent().find( '.active' ).removeClass( 'active' );
-          $( this ).addClass( 'active' );
+
+$(function() {
+    $('.side-button').on('click', function() {
+        $(this).parent().find('.active').removeClass('active');
+        $(this).addClass('active');
+        $('.card-1').css('background-color', 'rgb(' + (Math.random() * 256) + ',' + (Math.random() * 256) + ',' + (Math.random() * 256) + ')');
     });
 });
