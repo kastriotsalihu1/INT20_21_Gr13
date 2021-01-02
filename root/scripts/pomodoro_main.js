1eb56be5e041ccec00a1a19c31b98e15537fc9d6
niteqela1 <75045448+niteqela1@users.noreply.github.com>
2021-01-02 03:01:09 +0100

    variables for each button-js of pomodoro

---
+++ root/scripts/pomodoro_main.js
@@ -0,0 +1,13 @@
+var start =document.getElementById('start');
+var reset =document.getElementById('reset');
+var stop  =document.getElementById('stop');
+
+var wm =document.getElementById('work_minutes');
+var ws =document.getElementById('work_seconds');
+
+var bm =document.getElementById('break_minutes');
+var bs =document.getElementById('break_seconds');
+
+var startTimer;
+
+
