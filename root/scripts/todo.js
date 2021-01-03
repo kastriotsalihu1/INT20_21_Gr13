5ed501a25f6e78b1135ad8ea2dc3ad2303556158
LendritIbrahimi <35872021+LendritIbrahimi@users.noreply.github.com>
2021-01-03 07:07:38 +0100

    Finished main to-do card with minor bugs

---
+++ root/scripts/todo.js
@@ -0,0 +1,18 @@
+$(document).ready(function () {
+  $("#todoinput").keypress(function (event) {
+    var keycode = event.keyCode ? event.keyCode : event.which;
+    if (keycode == "13") {
+      $("#itemlist").prepend("<li>" + $("#todoinput").val() + "</li>");
+      $("#todoinput").val("");
+    }
+  });
+  $(document.body).on("click", "#itemlist > li", function () {
+    $(this).toggleClass("checked");
+  });
+
+  $('#displayinput').click(function(){
+    $('#todoinput').slideToggle('fast',function(){} );
+  });
+
+});
+
