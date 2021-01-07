aa6a358cb51a54a198f502dcd37b990472678744
niteqela1 <75045448+niteqela1@users.noreply.github.com>
2021-01-08 00:59:55 +0100

    declaring the  variables- music player

---
+++ root/scripts/music_player.js
@@ -0,0 +1,13 @@
+let previous = document.querySelector('#pre');
+let play = document.querySelector('#play');
+let next = document.querySelector('#next');
+let title = document.querySelector('#title');
+let recent_volume= document.querySelector('#volume');
+let volume_show = document.querySelector('#volume_show');
+let slider = document.querySelector('#duration_slider');
+let show_duration = document.querySelector('#show_duration');
+let track_image = document.querySelector('#track_image');
+let auto_play = document.querySelector('#auto');
+let present = document.querySelector('#present');
+let total = document.querySelector('#total');
+let artist = document.querySelector('#artist');
