5dc1e7b0f3ec0a830191e8c880a12c47329a7f85
LendritIbrahimi <35872021+LendritIbrahimi@users.noreply.github.com>
2021-01-11 21:06:28 +0100

    Finalizing the navigation and header loading

---
+++ root/scripts/load.js
@@ -0,0 +1,9 @@
+
+
+$.get("application_header.html", function(data){
+    $("#header").replaceWith(data);
+});
+
+$.get("application_sidebar.html", function(data){
+    $("#sidebar").replaceWith(data);
+});
