67ba27c93bd7c437be428980a73da37dab98645f
niteqela1 <75045448+niteqela1@users.noreply.github.com>
2021-01-11 18:12:01 +0100

    tree size

--- root/scripts/trees.js
+++ root/scripts/trees.js
@@ -3,7 +3,9 @@ const ctx = canvas.getContext('2d');
 const image = document.getElementById('tree_img');
 var Button = document.getElementById('btn');
 
+//onClick of btn add a rect
 
+ctx.fillStyle = "rgb(29, 18, 16)";
 
 Button.addEventListener('click', function(){
    ctx.fillRect(Math.random()*499,Math.random()*199,20,20)
@@ -13,3 +15,8 @@ Button.addEventListener('click', function(){
 
 
 
+//Button.addEventListener('dblclick',function(){
+
+//ctx.drawImage(image);
+
+//});
