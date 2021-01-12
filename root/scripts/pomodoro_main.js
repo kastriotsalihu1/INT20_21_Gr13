6409f58162c013707d3eca0aca1bb66351450b64
niteqela1 <75045448+niteqela1@users.noreply.github.com>
2021-01-12 03:29:01 +0100

    merged the trees script to pm script

--- root/scripts/pomodoro_main.js
+++ root/scripts/pomodoro_main.js
@@ -66,6 +66,22 @@ function timer1(){
         bs.innerText = "00";
 
         document.getElementById('counter').innerText++;
+        
+        if (document.getElementById('counter').innerText % 4 == 0) {
+            console.log("nita");
+                  let image = treeImages[treeIndex % treeImages.length];
+                  ctx.drawImage(
+                  image,
+                   xCoordinate[treeIndex % xCoordinate.length],
+                   yCoordinate[treeIndex % yCoordinate.length],
+                  image.width,
+                 image.height
+                  );
+                 treeIndex++;
+              
+                } 
+
+        
     }
 }
 
@@ -74,8 +90,69 @@ function stopInterval(){
     clearInterval(startTimer);
 }
 
+//JS FOR THE TREES' PART
+//JS FOR THE TREES' PART
+//JS FOR THE TREES' PART
+//JS FOR THE TREES' PART
+//JS FOR THE TREES' PART
+const treePaths = [
+    "images/trees/TreeFlat_1.png",
+      "images/trees/TreeFlat_2.png",
+      "images/trees/TreeFlat_3.png",
+      "images/trees/TreeFlat_4.png",
+      "images/trees/TreeFlat_5.png",
+      "images/trees/TreeFlat_6.png",
+      "images/trees/TreeFlat_7.png",
+      "images/trees/TreeFlat_8.png",
+  ];
+  var treeImages = [];
+  
+  for (let i = 0; i < 8; i++) {
+    treeImages.push(getImage(treePaths[i]));
+  }
+  // console.log(treePaths);
+  // console.log(treeImages);
+  
+  const canvas = document.getElementById("canvas");
+  const ctx = canvas.getContext("2d");
+  var button = document.getElementById("btn");
+  
+  
+  var xCoordinate = [];
+  var yCoordinate = [];
+  let canvasWidth = 500,
+    canvasHeight = 200,
+    resizingFactor = 1,
+    imageH = 40,
+    imageW = 40;
+  for (let i = 0; i < canvasWidth / imageW; i += 1) xCoordinate[i] = i * imageW;
+  for (let i = 0; i < canvasHeight / imageW; i += 1) yCoordinate[i] = i * imageH;
+  
+  xCoordinate = shuffle(xCoordinate);
+  yCoordinate = shuffle(yCoordinate);
+  
+  console.log(xCoordinate, yCoordinate);
+  
+  
+  var treeIndex = 0;
 
-
-
-
-
+  function shuffle(array) {
+    var tmp,
+      current,
+      top = array.length;
+    if (top)
+      while (--top) {
+        current = Math.floor(Math.random() * (top + 1));
+        tmp = array[current];
+        array[current] = array[top];
+        array[top] = tmp;
+      }
+    return array;
+  }
+  
+  function getImage(src) {
+    let image = new Image();
+    image.src = src;
+    return image;
+  }
+  
