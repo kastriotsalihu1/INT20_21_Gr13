954b4dcdf78484bc8eb005f67ee4ad91fdf34b73
niteqela1 <75045448+niteqela1@users.noreply.github.com>
2021-05-15 20:50:04 +0200

    login db connection and validation

---
+++ root/login/login.php
@@ -0,0 +1,59 @@
+<?php
+require_once("dbConfig.php");
+require("functions.php");
+if(isset($_POST['login'])){
+  $con= dbConfig::connect();
+ 
+  $username = funksioni::validateString($_POST['username']);
+  $password = funksioni::hashedPw($_POST['pw']);
+  
+  if(funksioni::checkLogin($con, $username, $password)){
+    $_SESSION['username']= $username;
+    header("Location:profile.php");
+  }else{
+    echo"Username or password is incorrect!";
+  }
+ 
+}
+
+?>
+
+<!DOCTYPE html>
+<html>
+  <head>
+  <title>LogIn</title>
+  <link rel="stylesheet" href="styles/login.css">
+  <!-- Simple css to remove the watermark from the hosting website -->
+  <link rel="stylesheet" href="styles/removewatermark.css">
+  <!-- Simple css to remove the watermark from the hosting website -->
+
+  </head>
+  <body>
+     <div class="hero">
+       <div class="form-box">
+          <div class="button-box">
+            <div id="btn"> </div>
+             <button type="button" class="toggle-btn" id="one" >Sign in</button>
+             <button type="button" class="toggle-btn" id="two"><a href="register.php">Sign up</a></button>
+          </div>
+          <div class="icon">
+            <a href="index.html">
+            <img src="images/wp_img/logo.png" width="80px"  id="icon" alt="User Icon" >
+          </a>
+          </div>
+         <form  id="login" class="input-group" name="login" method="post" action=""  >
+              <input type="text" class="input-field" placeholder="Username"   name="username" autofocus>
+              <input type="Password" name="pw" class="input-field"   placeholder="Password" autofocus >
+              <button type="submit"  name="login"  class="submit-btn"><b>Sign in
+              </b></button>
+              <div id="formFooter">
+                <a class="underlineHover" href="#">Forgot Password?</a>
+              </div>
+         </form>
+        
+       </div>
+     </div>
+     
+  </body>
+</html>
+
