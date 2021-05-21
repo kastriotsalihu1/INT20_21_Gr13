bf8e8e446a3158729b501d1755e2037fee39e974
LendritIbrahimi <35872021+LendritIbrahimi@users.noreply.github.com>
2021-05-21 22:59:43 +0200

    Can now add and save notes

--- root/dbConfig.php
+++ root/dbConfig.php
@@ -1,22 +1,29 @@
-<?php  
+<?php
 
-class dbConfig{
-public static function connect(){
-        define("servername", "localhost");
-        define("username", "root");
-        define("password", "");
-        define("dbname", "estudent");
-    try{
-        $con = new PDO("mysql:host=localhost; dbname=estudent", username, password);
-        $con->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);    
+class dbConfig
+{
+    public static function connect()
+    {
+        if (!defined('servername')) {
+            define("servername", "localhost");
+        }
+        if (!defined('username')) {
+            define("username", "root");
+        }
+        if (!defined('password')) {
+            define("password", "");
+        }
+        if (!defined('dbname')) {
+            define("dbname", "estudent");
+        }
+        try {
+            $con = new PDO("mysql:host=localhost; dbname=estudent", username, password);
+            $con->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        } catch (PDOException $e) {
+            echo $e->getMessage();
+        }
+        return $con;
     }
-    catch(PDOException $e){
-        echo $e->getMessage();
-    }
-    return $con;
-}
-
-
 }
 
 ?>
