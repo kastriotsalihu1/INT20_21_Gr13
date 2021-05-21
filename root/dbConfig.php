e2bf914783054c37a360903c1e4056871a15b81c
LendritIbrahimi <35872021+LendritIbrahimi@users.noreply.github.com>
2021-05-21 23:02:45 +0200

    Changed to dbConfig

--- root/dbConfig.php
+++ root/dbConfig.php
@@ -20,6 +20,14 @@ class dbConfig
             $con = new PDO("mysql:host=localhost; dbname=estudent", username, password);
             $con->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
             $con->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
+        }catch (PDOException $e) {
+            echo $e->getMessage();
         }
+        return $con;
     }
 }
+
+?>
+
+
+
